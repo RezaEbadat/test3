aa40df883f4656ff23f50e6ea07eee0ce77f3efa
Reza Ebadat <rezaebadat@yahoo.com>
2024-06-01 14:07:01 +0330

    add h2 text

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    
+    
+</head>
+<body dir="rtl">
+
+<h2?php
+         include "header.php";
+?>
+<p  style="font-size:80px; font-color: black; margin-right: 650px; margin-left: 650px; margin-top: 50px">
+     <?php
+          echo "Wellcome"; 
+          echo "reza";
+          echo "ali";
+          echo "mohsen";
+          echo "new";
+          
+
+
+
+     ?> 
+     <h1>text</h1>
+     <h1>res</h1>
+     <h2>this is a sample text</h2>
+     
+</p>
+</body>
+</html>
